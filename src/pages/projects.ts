5f5eb4777868c1f3e28f80806d48dae623634a25
Dan Fishgold <d.fishgold@gmail.com>
2023-11-21 16:26:22 +0200

    Add an English homepage

--- src/pages/projects.ts
+++ src/pages/projects.ts
@@ -32,3 +32,70 @@ export const hebrewProjects: Project[] = [
     url: null,
   },
 ]
+
+export const englishProjects: Project[] = [
+  {
+    title: 'Bike Map',
+    description:
+      'An up to date map of bike lanes and recommended routes in Tel Aviv. Designed for mobile',
+    width: 2,
+    url: 'https://bikemap.dan.city',
+  },
+  {
+    title: 'Community Involvement Bot',
+    description:
+      'A Telegram channel that notifies about community involvement events organized by the Tel Aviv municipality',
+    width: 3,
+    url: 'https://dan.city/shituf-bot/',
+  },
+  {
+    title: 'Code Reading Club',
+    description:
+      'Workshops, exercises and techniques to help you understand your code better!',
+    width: 5,
+    url: 'https://codereading.club',
+  },
+  {
+    title: 'Sign Language Dictionary',
+    description: 'An interactive dictionary to help learn Sign Language (ISL)',
+    width: 3,
+    url: 'https://isl.danfishgold.com/#en',
+  },
+  {
+    title: 'Wikipedia Charts',
+    description: 'The 100 most visited pages on Wikipedia on any given day',
+    width: 2,
+    url: 'https://wikipedia-charts.danfishgold.com',
+  },
+  {
+    title: 'Emoji Poetry',
+    description: 'Generate poems in emoji!',
+    width: 2,
+    url: 'https://emoji-poetry.danfishgold.com',
+  },
+  {
+    title: 'Patterns',
+    description: '30 Japanese patterns recreated in p5.js',
+    width: 3,
+    url: 'https://codecember19.danfishgold.com',
+  },
+  {
+    title: 'Magnets',
+    description: 'A weird interactive Hebrew linguistics explainer',
+    width: 3,
+    url: 'https://files.dan.city/magnets',
+  },
+  {
+    title: 'Processing',
+    description: '30 generative art projects in Processing',
+    width: 2,
+    url: 'https://codecember18.danfishgold.com',
+  },
+  {
+    title: 'Trains',
+    description:
+      "An iOS app from 2016 showing the time table for the Israeli train. The app is dead now and all that's left is this website I made for it",
+    width: 5,
+    url: 'https://files.dan.city/trains',
+  },
+]
