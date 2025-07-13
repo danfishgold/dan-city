31fa5c0df6353a28211de46926257283fcd19f7d
Dan Fishgold <d.fishgold@gmail.com>
2025-07-13 10:19:11 +0200

    Remove unnecessary stuff from previous attempts and fix(?) lazy loading

--- astro.config.ts
+++ astro.config.ts
@@ -1,19 +1,8 @@
 import mdx from "@astrojs/mdx";
 import { defineConfig } from "astro/config";
-import path from "node:path";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    mdx(),
-    {
-      name: "add-story-collection-to-watched-files",
-      hooks: {
-        "astro:config:setup": ({ addWatchFile }) => {
-          addWatchFile(path.resolve("./src/stories/index.ts"));
-        },
-      },
-    },
-  ],
+  integrations: [mdx()],
   site: process.env.URL || "http://localhost:3000",
 });
